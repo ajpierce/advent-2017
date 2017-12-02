abc6c03c23038bc8ea0afb8257122800f0fbaa9e
Andrew J. Pierce <apierce@openwhere.com>
2017-12-01 21:47:52 -0500

    Refactor day-01 part-1 to use (map vector) instead of partition
    This also fixes a bug where an incorrect value would be calculated when
    using an input code that has an odd number of digits

--- src/advent_2017/day_01/part_1.clj
+++ src/advent_2017/day_01/part_1.clj
@@ -1,33 +1,20 @@
 (ns advent-2017.day-01.part-1
-  "The captcha requires you to review a sequence of digits (your puzzle input) 
-  and find the sum of all digits that match the next digit in the list. The list 
-  is circular, so the digit after the last digit is the first digit in the list.")
+  "The captcha requires you to review a sequence of digits (your puzzle input)
+  and find the sum of all digits that match the next digit in the list. The list
+  is circular, so the digit after the last digit is the first digit in the list."
+  (:require
+    [advent-2017.day-01.core :as core :refer [split-and-test str->digits tests1]]))
 
-; Test values (and answers) presented in the problem text
-(def tests [[1122 3] 
-            [1111 4] 
-            [1234 0] 
-            [91212129 9]])
+(defn get-digits [input]
+  (->> input
+       str->digits
+       ((fn [xs] (map vector xs (rest (cycle xs)))))
+       (filter (fn [[a b]] (= a b)))
+       (map first)))
 
-(defn get-pairs [input]
-  (let [a (->> input str seq vec)
-        b (conj (subvec a 1) (first a))]
-    (->> b (concat a) (partition 2))))
-
-(defn filter-pairs [pairs]
-  (filter (fn [[a b]] (= a b)) pairs))
-
-(defn sum-pairs [pairs]
-  (->> pairs
-       (map first)
-       (map #(Character/digit % 10))
+(defn calculate [input]
+  (->> input
+       get-digits
        (reduce +)))
 
-(defn calculate [code] (->> code get-pairs filter-pairs sum-pairs))
-
-(defn split-and-test [[code expected]]
-  (let [actual (calculate code)
-        match? (= expected actual)]
-    (println "Does" code "yield" expected "?" match?)))
-
-(map split-and-test tests)
+(map #(split-and-test calculate %) tests)
